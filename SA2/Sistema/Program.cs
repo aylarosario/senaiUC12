f27de6a4041f3f256cba60a2fcbcd09e3fc86aff
Ayla <98574408+aylarosario@users.noreply.github.com>
2022-10-25 19:42:44 -0300

    ER6

--- SA2/Sistema/Program.cs
+++ SA2/Sistema/Program.cs
@@ -2,6 +2,22 @@
 public class program{
 static void Main(string[] args)
 {
+    static void BarraCarregamento(string texto){
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write(texto);
+                Thread.Sleep(500);
+                Console.Write("#");
+
+                for (var contador = 0; contador < 10; contador++)
+                {
+                    Console.Write("#");
+                    Thread.Sleep(500);
+                }
+                Console.ResetColor();
+            }
+
+
     Console.ForegroundColor = ConsoleColor.DarkCyan;
     Console.WriteLine(@$"
     ===============================================
@@ -14,16 +30,7 @@ static void Main(string[] args)
     ");
     Console.ResetColor();
 
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.Write("iniciando ");
-    Thread.Sleep(500);
-    Console.Write("#");
-
-    for(var contador = 0; contador < 10; contador++){
-        Console.Write("#");
-        Thread.Sleep(500);
-    }
-    Console.ResetColor();
+    BarraCarregamento("Iniciando ");
 
     string opcao;
     do{
@@ -40,20 +47,62 @@ static void Main(string[] args)
     !                                             !
     ===============================================
     ");
+    Console.ResetColor();
     opcao = Console.ReadLine();
 
-    switch(opcao){
-        case "1":
-        break;
-        case "2":
-        break;
-        case "0":
-        break;
-        default:
-        Console.WriteLine("Opção inválida, por favor digite uma das opções apresentadas");
-        break;
-    }
-    }while(opcao != "0");
+                switch (opcao)
+                {
+                    case "1":
+                    Endereco endPf = new Endereco();
+                    endPf.logradouro = "Rua Niteroi";
+                    endPf.numero = 180;
+                    endPf.complemento = "Escola SENAI Paulo Skaf";
+                    endPf.enderecoComercial = false;
+
+                    PessoaFisica Ayla = new PessoaFisica();
+                    Ayla.CPF = "03359673278";
+                    Ayla.dataNascimento = new DateTime(1998, 09, 24);
+                    Ayla.endereco = endPf;
+                    Ayla.nome = "Ayla Rayane dos Santos do Rosário";
+
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    Console.WriteLine(@$"Nome: {Ayla.nome}
+CPF: {Ayla.CPF}
+Nascido em {Ayla.dataNascimento.ToString("dd/MM/yyyy")}
+{endPf.logradouro}, {endPf.numero}");
+
+                    Console.ResetColor();
+                        break;
+
+                    case "2":
+                    Endereco endPj = new Endereco();
+                    endPj.logradouro = "Rua Niteroi";
+                    endPj.numero = 180;
+                    endPj.complemento = "Escola SENAI Paulo Skaf";
+                    endPj.enderecoComercial = true;
+
+                    PessoaJuridica pj = new PessoaJuridica();
+                    pj.CNPJ = "0141154798";
+                    pj.endereco = endPj;
+                    pj.razaoSocial = "Pessoa Juridica";
+                    pj.nome = "Jequiti";
+
+                    Console.WriteLine(pj.CNPJ);
+
+                        break;
+
+                    case "0":
+                        Console.WriteLine("Obrigado por utilizar nosso sistema");
+                        BarraCarregamento("Finalizando ");
+                        break;
+
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Opção inválida, por favor digite uma das opções apresentadas");
+                        Console.ResetColor();
+                        break;
+                }
+            } while (opcao != "0");
 
     Console.ResetColor();
 }
